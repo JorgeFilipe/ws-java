7b8efa329923a1c842b60801e92558ab3b0b1b28
Jorge Filipe da Silva Macedo <jorgefilipedasilva@gmail.com>
2022-12-27 19:54:27 -0300

    Create Main.java

---
+++ fundamentals/Vetores/vetor01/Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    
+    String mes[] = {"Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto", "Setembro","Outubro","Novembro","Dezembro"};
+    int diasMes[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    int i;
+    for (i=0; i<12; i++){
+      System.out.printf("%s, tem %d dias. \n", mes[i], diasMes[i]);
+    }
+  }
+}
