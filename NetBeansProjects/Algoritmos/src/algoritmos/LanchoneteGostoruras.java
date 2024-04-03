fa2cf0398e17e654a7cfaf71efd487897239c129
Jorge Macedo <jorgefilipedasilva@gmail.com>
2024-04-02 22:56:56 -0300

    Create LanchoneteGostoruras.java

---
+++ NetBeansProjects/Algoritmos/src/algoritmos/LanchoneteGostoruras.java
@@ -0,0 +1,25 @@
+/*
+ 
+16. A lanchonete Gostosura vende apenas um tipo de sanduíche, cujo recheio inclui duas fatias de 
+queijo, uma fatia de presunto e uma rodela de hambúrguer. Sabendo que cada fatia de queijo ou 
+presunto pesa 50 gramas, e que a rodela de hambúrguer pesa 100 gramas, faça um algoritmo em 
+que o dono forneça a quantidade de sanduíches a fazer, e a máquina informe as quantidades (em 
+quilos) de queijo, presunto e carne necessários para compra. 
+ 
+*/
+package algoritmos;
+import java.io.PrintStream;
+import java.lang.System;
+import java.util.Scanner;
+/**
+ *
+ * @author jorge
+ */
+public class LanchoneteGostoruras {
+    
+     public static void main(String[] args) {
+         Scanner read = new Scanner(System.in);
+        System.out.println(" ----( Lanchonete Gostosura )---- ");
+        System.out.println("");
+    }
+}
